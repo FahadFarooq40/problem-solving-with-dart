c8672f0843df2a2614dd7229726891417d83e8ea
Fahad Farooq <104043766+FahadFarooq40@users.noreply.github.com>
2025-10-20 17:30:16 +0500

    Create for_loop_practice.dart

---
+++ for_loop_practice.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  // simple Loop
+
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  //  Array me loop
+
+  List numbers = [10, 20, 90, 40, 50];
+
+  for (int i = 0; i < numbers.length; i++) {
+    print(numbers[i]);
+  }
+
+  // For-Each Loop
+
+  List<int> number = [10, 20, 90, 40, 50];
+  for (var num in number) {
+    print(num);
+  }
+
+  String? input = stdin.readLineSync();
+
+  int numberr = int.parse(input!);
+  int sum = 0;
+  for (int i = 1; i <= numberr; i++) {
+    sum += i;
+  }
+  print("Sum = $sum");
+}
